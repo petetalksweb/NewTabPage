8f3e1338c5838075211050ca91a4e8b1ea90e70c
jacobpetersen <jacob@jacob-petersen.com>
2016-09-03 16:41:50 -0500

    PeteTalksWeb: Templating Twitter trends links

--- js/Twitter.js
+++ js/Twitter.js
@@ -1,38 +1,18 @@
 function getTwitterInformation() {
-    ajaxGetJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Ftwitter.com%2Fi%2Ftrends%3Fk%3D2316700e79%26pc%3Dtrue%26show_context%3Dfalse%26src%3Dsearch-home%22&format=json&callback=").then(function(twitterResponse) {
-        var htmlString = decodeURI(JSON.parse(twitterResponse.query.results.body).module_html);
-        var listItems = htmlString.split('</li>');
-        var trends = [];
-        for(var i = 0; i < 10; i++) {
-            trends.push(parseTwitterTrend(listItems[i]));
-        }
-        generateAllTwitterTrendHTML(trends);
+    ajaxGetJSON('https://morbi1lqbb.execute-api.us-west-2.amazonaws.com/dev/twitter').then(function(twitterResponse) {
+        console.log(twitterResponse);
+        var twitterTrendTextLinkObjects = convertTwitterTrendsToTextLinkObjects(twitterResponse.trends);
+        generateTextLinks(twitterTrendTextLinkObjects, 'twitter');
     })
 }
 
-function parseTwitterTrend(listItem) {
-    var newTrend = {};
-    newTrend.name = listItem.split('data-trend-name="')[1].split('"')[0];
-    newTrend.href = 'http://twitter.com' + listItem.split('href="')[1].split('" data-query')[0];
-    return newTrend;
-}
-
-function generateAllTwitterTrendHTML(twitterTrends) {
+function convertTwitterTrendsToTextLinkObjects(twitterTrends) {
+    var textLinkObjects = [];
     for(var i = 0; i < twitterTrends.length; i++) {
-        generateTwitterTrendHTML(twitterTrends[i]);
+        var currentTextLinkObject = {};
+        currentTextLinkObject.title = twitterTrends[i].name;
+        currentTextLinkObject.href = twitterTrends[i].url;
+        textLinkObjects.push(currentTextLinkObject);
     }
-}
-
-function generateTwitterTrendHTML(twitterTrend) {
-    var twitterTrendDiv = document.createElement('div');
-    twitterTrendDiv.className = 'hackerNewsLink';
-    twitterTrendDiv.appendChild(generateTwitterTrendLink(twitterTrend));
-    document.getElementById('twitter').appendChild(twitterTrendDiv);
-}
-
-function generateTwitterTrendLink(twitterTrend) {
-    var twitterTrendLink = document.createElement('a');
-    twitterTrendLink.href = twitterTrend.href;
-    twitterTrendLink.innerHTML = twitterTrend.name;
-    return twitterTrendLink;
+    return textLinkObjects;
 }
