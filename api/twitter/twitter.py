3d91e52758dd42d0f075dbdd4ea32950b7f696a9
jacobpetersen <jacob@jacob-petersen.com>
2016-09-02 22:28:10 -0500

    PeteTalksWeb: Python function to get Twitter Data through Twitter API

---
+++ api/twitter/twitter.py
@@ -0,0 +1,27 @@
+from twitter import *
+import json
+
+def getTrends(event, context):
+    twitterTokens = getTwitterTokens()
+    accessKey = twitterTokens[0]
+    accessSecret = twitterTokens[1]
+    consumerKey = twitterTokens[2]
+    consumerSecret = twitterTokens[3]
+
+    twitter = Twitter(auth = OAuth(accessKey, accessSecret, consumerKey, consumerSecret))
+
+    trendsResponse = twitter.trends.place(_id = 23424977)
+
+    trends = trendsResponse[0]["trends"][:10]
+
+    return {
+        'trends' : trends
+    }
+
+def getTwitterTokens():
+    twitterTokens = []
+    with open('twitterTokens.txt') as tokensFile:
+        for token in tokensFile:
+            twitterTokens.append(token.strip())
+
+    return twitterTokens
