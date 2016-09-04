c814b80d3a2e03d6b8bbb98ff46daab595f6255b
jacobpetersen <jacob@jacob-petersen.com>
2016-09-04 00:02:00 -0500

    PeteTalksWeb: Adding rss link add/remove using localstorage

--- js/RSS.js
+++ js/RSS.js
@@ -1,8 +1,64 @@
-function getRSSFeeds(urls, elementID) {
+var rssURLS = {
+    blogs: ['http://daverupert.com/atom.xml',
+        'https://jakearchibald.com/posts.rss',
+        'http://feeds.igvita.com/igvita',
+        'http://feeds.feedburner.com/paul-irish?format=xml',
+        'http://feeds.feedburner.com/CssTricks?format=xml',
+        'https://hacks.mozilla.org/feed/',
+        'https://jslive.com/rss'],
+    news:  ['http://rss.cnn.com/rss/cnn_topstories.rss?format=xml',
+            'http://www.wired.com/feed/',
+            'https://feeds.bbci.co.uk/news/rss.xml?edition=us',
+            'http://feeds.feedburner.com/TechCrunch/?format=xml',
+            'http://america.aljazeera.com/content/ajam/articles/rss',
+        ]
+};
+
+function getRSSFeeds(collectionName, modalName, settingsIconName, closeIconName, rssURLInputDivID) {
+    if(!localStorage.getItem(collectionName)) {
+        localStorage.setItem(collectionName, JSON.stringify(rssURLS[collectionName]));
+    }
+    var urls = JSON.parse(localStorage.getItem(collectionName));
     var yqlURL = generateYQLURL(urls);
     ajaxGetJSON(yqlURL).then(function(yqlResponse) {
-        generateAllBlogPostHTML(yqlResponse.query.results.feed, elementID)
+        generateAllBlogPostHTML(yqlResponse.query.results.feed, collectionName)
     });
+    setupModal(modalName, settingsIconName, closeIconName);
+    generateRssURLInputs(urls, rssURLInputDivID);
+}
+
+function deleteURL() {
+    var div = event.srcElement.parentNode;
+    div.parentNode.removeChild(div);
+}
+
+function addRssURLInput(appendToID) {
+    var rssUrlInputTemplate = document.getElementById('rssURLInput');
+    var containingDiv = document.getElementById(appendToID);
+    var newRssUrlInputDiv = rssUrlInputTemplate.content.cloneNode(true);
+    var newRssUrlInput = newRssUrlInputDiv.querySelector('input');
+    newRssUrlInput.placeholder = 'New RSS URL';
+    containingDiv.appendChild(newRssUrlInputDiv);
+}
+
+function saveRssURLs(collectionName) {
+    var rssURLInputs = event.srcElement.parentNode.getElementsByTagName('input');
+    var rssURLsToSave = [];
+    for(var i = 0; i < rssURLInputs.length; i++) {
+        rssURLsToSave.push(rssURLInputs[i].value);
+    }
+    localStorage.setItem(collectionName, JSON.stringify(rssURLsToSave));
+}
+
+function generateRssURLInputs(rssURLs, appendToID) {
+    var rssUrlInputTemplate = document.getElementById('rssURLInput');
+    var containingDiv = document.getElementById(appendToID);
+    for(var i = 0; i < rssURLs.length; i++) {
+        var currentRssUrlInputDiv = rssUrlInputTemplate.content.cloneNode(true);
+        var rssURLInput = currentRssUrlInputDiv.querySelector('input');
+        rssURLInput.value = rssURLs[i];
+        containingDiv.appendChild(currentRssUrlInputDiv);
+    }
 }
 
 function generateYQLURL(urls) {
--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
-var devRSSURLs = ['http://daverupert.com/atom.xml',
-    'https://jakearchibald.com/posts.rss',
-    'http://feeds.igvita.com/igvita',
-    'http://feeds.feedburner.com/paul-irish?format=xml',
-    'http://feeds.feedburner.com/CssTricks?format=xml',
-    'https://blog.codepen.io/feed/',
-    'https://hacks.mozilla.org/feed/',
-    'https://jslive.com/rss'
-];
-var newsRSSURLs = ['http://rss.cnn.com/rss/cnn_topstories.rss?format=xml',
-    'http://www.wired.com/feed/',
-    'https://feeds.bbci.co.uk/news/rss.xml?edition=us',
-    'http://feeds.feedburner.com/TechCrunch/?format=xml',
-    'http://america.aljazeera.com/content/ajam/articles/rss',
-];
-getWeatherInformation();
-getHackerNewInformation();
-getVestInformation();
-getCountdownTimeInformation();
-getWikipeidaInformation();
-getRSSFeeds(devRSSURLs, 'devRSS');
-getRSSFeeds(newsRSSURLs, 'newsRSS');
-getRedditInformation();
-getTwitterInformation();
+// getWeatherInformation();
+// getHackerNewInformation();
+// getVestInformation();
+// getCountdownTimeInformation();
+// getWikipeidaInformation();
+getRSSFeeds('blogs', 'blogInformationModal', 'blogSettingsIcon', 'blogSettingsClose', 'blogRssUrlInputDiv');
+getRSSFeeds('news', 'newsInformationModal', 'newsSettingsIcon', 'newsSettingsClose', 'newsRssUrlInputDiv');
+// getRedditInformation();
+// getTwitterInformation();
