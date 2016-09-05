497fc892907865836d3b43120dc667c6601f629a
jacobpetersen <jacob@jacob-petersen.com>
2016-09-05 00:16:52 -0500

    PeteTalksWeb: Adding localStorage caching of ajax

--- js/AJAXPromises.js
+++ js/AJAXPromises.js
@@ -27,6 +27,31 @@ function ajaxGetJSON(url) {
     });
 }
 
+function ajaxGetJSONWithCaching(url, cacheTimeHours) {
+    if(localStorage.getItem(url)) {
+        var cachedItem = JSON.parse(localStorage.getItem(url));
+        if((Math.abs(new Date(cachedItem.cachedAt) - new Date()) / 36e5) <= cacheTimeHours) {
+            return new Promise(function(resolve, reject) {
+                resolve(cachedItem.response);
+            }).catch(function(error) {
+                console.error(error);
+            });
+        }
+    }
+    return new Promise(function(resolve, reject) {
+        ajaxGetJSON(url).then(function(jsonResponse) {
+            var cachedItem = {
+                cachedAt: new Date(),
+                response: jsonResponse
+            };
+            localStorage.setItem(url, JSON.stringify(cachedItem));
+            resolve(jsonResponse);
+        }).catch(function(error) {
+            console.error(error);
+        });
+    });
+}
+
 function ajaxGetJSONP(url, callback) {
     var jsonpScript = document.createElement('script');
     jsonpScript.setAttribute('src', url + '&callback=' + callback);
